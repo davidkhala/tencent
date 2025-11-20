47b4e41d9d19a51abcaf356c28f25739fa64783a
David Liu <david-khala@hotmail.com>
2025-11-20 21:37:14 +0800

    backup

--- cloud/tsf/agent.sh
+++ cloud/tsf/agent.sh
@@ -9,7 +9,7 @@ if [ -f tsf-agent/ops/uninstall.sh ]; then
 	tsf-agent/ops/uninstall.sh
 fi 
 rm -rf tsf-agent* 
-wget http://169.254.0.77:8100/tsf/component/tsf-agent.tar.gz -O tsf-agent.tar.gz
-tar --no-same-owner -xzf tsf-agent.tar.gz 
+wget http://169.254.0.77:8100/tsf/component/tsf-agent.tar.gz
+tar --no-same-owner -xzf tsf-agent.tar.gz
 chmod +x tsf-agent/ops/install.sh
 tsf-agent/ops/install.sh
